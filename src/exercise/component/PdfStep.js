2de56bd3a6f51a39d3a4dc7b1d5d07c38476a70f
Jordan Marquez <jordan.marquez@blueflag.com.au>
2018-07-13 17:07:56 +1000

    add window.open function

--- src/exercise/component/PdfStep.js
+++ src/exercise/component/PdfStep.js
@@ -76,8 +76,11 @@ class PdfStep extends React.PureComponent<Props, State> {
         document
             .querySelectorAll(".react-pdf__Page a")
             .forEach((elem: *) => {
-                elem.href = elem.href.replace("https://", "http://");
-                elem.target = "_blank";
+                elem.addEventListener('click', function(event) {
+                    window.alert("PLEASEEEE GET ME!!!")
+                    window.open(elem.href, "_blank");
+                    event.preventDefault();
+                });
             });
     }
 
